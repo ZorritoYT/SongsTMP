11d40049d76251421ad11de4231deccb016afc20
ZorritoYT <69325535+ZorritoYT@users.noreply.github.com>
2021-01-04 23:38:50 -0400

    Update dbSONGS.bat

--- dbSONGS.bat
+++ dbSONGS.bat
@@ -17,14 +17,19 @@ echo.
 if not exist "laresaka-supermerk2.mp3" "%locate%\batbox" /c 0x4 /d "echo 1) laresaka-supermerk2 (Requiere download)"
 if exist "laresaka-supermerk2.mp3" "%locate%\batbox" /c 0xa /d "echo 1) laresaka-supermerk2"
 echo.
+if not exist "MegalovaniaPianoCover.mp3" "%locate%\batbox" /c 0x4 /d "echo 2) Megalovania Undertale (Piano Cover) [Requiere download]"
+if exist "MegalovaniaPianoCover.mp3" "%locate%\batbox" /c 0xa /d "echo 2) Megalovania Undertale [Piano Cover]"
+echo.
 echo.
 set/p input="SongsDB/> "
 if "%input%"=="return" exit
 if "%input%"=="stop" goto endS
-if exist "laresaka-supermerk2.mp3" start dlc -w -p "laresaka-supermerk2.mp3"
+if exist "laresaka-supermerk2.mp3" if "%input%"=="1" start dlc -w -p "laresaka-supermerk2.mp3"
 if not exist "laresaka-supermerk2.mp3" if "%input%"=="1" wget https://github.com/ZorritoYT/SongsTMP/raw/main/laresaka-supermerk2.mp3 & del /q .wget-hsts & start dlc -w -p "laresaka-supermerk2.mp3"
-goto loop
 
+if exist "MegalovaniaPianoCover.mp3" if "%input%"=="2" start dlc -w -p "MegalovaniaPianoCover.mp3"
+if not exist "MegalovaniaPianoCover.mp3" if "%input%"=="2" wget https://github.com/ZorritoYT/SongsTMP/raw/main/MegalovaniaPianoCover.mp3 & del /q .wget-hsts & start dlc -w -p "MegalovaniaPianoCover.mp3"
+goto loop
 :endS
 taskkill /f /im dlc.exe /t
 taskkill /f /im "dlc.exe *32" /t
