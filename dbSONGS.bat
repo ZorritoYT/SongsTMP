3d63fc18eced680ddae2b15dbc4dbf36681b2239
ZorritoYT <69325535+ZorritoYT@users.noreply.github.com>
2021-01-04 20:28:34 -0400

    Update dbSONGS.bat

--- dbSONGS.bat
+++ dbSONGS.bat
@@ -1,4 +1,5 @@
 @echo off & color a
+title SongDB
 mode 120,30
 set 1=laresaka-supermerk2.mp3
 set 1x=https://github.com/ZorritoYT/SongsTMP/raw/main/laresaka-supermerk2.mp3
@@ -6,4 +7,8 @@ cls
 echo.
 echo Escribe el numero de la cancion a descargar
 echo.
-echo 1) La resaka - SuperMark2
+echo Tambien puedes escribir "return" para salir
+echo.
+echo 1) laresaka-supermerk2
+echo.
+set/p input="SongsDB/> "
