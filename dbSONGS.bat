1013ed2e9adbc6c4886e85717e7e3da6739962e7
ZorritoYT <69325535+ZorritoYT@users.noreply.github.com>
2021-01-04 20:27:19 -0400

    Update dbSONGS.bat

--- dbSONGS.bat
+++ dbSONGS.bat
@@ -1,4 +1,9 @@
 @echo off & color a
+mode 120,30
 set 1=laresaka-supermerk2.mp3
 set 1x=https://github.com/ZorritoYT/SongsTMP/raw/main/laresaka-supermerk2.mp3
 cls
+echo.
+echo Escribe el numero de la cancion a descargar
+echo.
+echo 1) La resaka - SuperMark2
