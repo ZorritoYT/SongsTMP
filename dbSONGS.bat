efd840c687f8eb9b76bc19eea0610a1377f2a0e2
ZorritoYT <69325535+ZorritoYT@users.noreply.github.com>
2021-01-04 20:36:53 -0400

    Update dbSONGS.bat

--- dbSONGS.bat
+++ dbSONGS.bat
@@ -3,9 +3,7 @@ set locate=%1
 @echo off & color a
 title SongDB
 mode 120,30
-set 1=laresaka-supermerk2.mp3
-set 1x=https://github.com/ZorritoYT/SongsTMP/raw/main/laresaka-supermerk2.mp3
-cd "%locate%"
+cd "%locate%\Files\Sounds"
 cls
 echo.
 echo Escribe el numero de la cancion a descargar
@@ -15,4 +13,7 @@ echo.
 echo 1) laresaka-supermerk2
 echo.
 set/p input="SongsDB/> "
-if "%input%"==1 
+if "%input%"==return exit
+if exist "laresaka-supermerk2.mp3" start dlc -w -p "laresaka-supermerk2.mp3"
+if not exist "laresaka-supermerk2.mp3" if "%input%"==1 "%locate%\wget" https://github.com/ZorritoYT/SongsTMP/raw/main/laresaka-supermerk2.mp3
+exit
