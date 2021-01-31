5d28cd260c46556f3fe877b99e070af7afa6514d
ZorritoYT <69325535+ZorritoYT@users.noreply.github.com>
2021-01-31 11:06:19 -0400

    Update dbSONGS.bat

--- dbSONGS.bat
+++ dbSONGS.bat
@@ -28,6 +28,9 @@ echo.
 if not exist "DeadToMe.mp3" "%locate%\batbox" /c 0x4 /d " 4) Dead To Me (feat. Lox Chatterbox)"
 if exist "DeadToMe.mp3" "%locate%\batbox" /c 0xa /d " 4) Dead To Me (feat. Lox Chatterbox)"
 echo.
+if not exist "SunFlower.mp3" "%locate%\batbox" /c 0x4 /d " 5) Post Malone, Swae Lee - Sunflower"
+if exist "SunFlower.mp3" "%locate%\batbox" /c 0xa /d " 5) Post Malone, Swae Lee - Sunflower"
+echo.
 echo.
 set/p input="SongsDB/> "
 if "%input%"=="return" goto :eof
@@ -41,8 +44,11 @@ if not exist "MegalovaniaPianoCover.mp3" if "%input%"=="2" wget https://github.c
 if exist "Laur-Sound-Chimera.mp3" if "%input%"=="3" start dlc -w -p "Laur-Sound-Chimera.mp3"
 if not exist "Laur-Sound-Chimera.mp3" if "%input%"=="3" wget https://github.com/ZorritoYT/SongsTMP/raw/main/Laur-Sound-Chimera.mp3 & del /q .wget-hsts & start dlc -w -p "Laur-Sound-Chimera.mp3"
 
-if exist "DeadToMe.mp3" if "%input%"=="4" start dlc -w -p "DeadToMe.mp3" & start /b cmdbkg "%locate%\Files\Sounds\DeadToMe.bmp" 1
+if exist "DeadToMe.mp3" if "%input%"=="4" start dlc -w -p "DeadToMe.mp3"
 if not exist "DeadToMe.mp3" if "%input%"=="4" wget https://github.com/ZorritoYT/SongsTMP/raw/main/DeadToMe.mp3 & del /q .wget-hsts & start dlc -w -p "DeadToMe.mp3"
+
+if exist "SunFlower.mp3" if "%input%"=="5" start dlc -w -p "SunFlower.mp3"
+if not exist "SunFlower.mp3" if "%input%"=="5" wget https://github.com/ZorritoYT/SongsTMP/raw/main/SunFlower.mp3 & del /q .wget-hsts & start dlc -w -p "SunFlower.mp3"
 
 goto loop
 
